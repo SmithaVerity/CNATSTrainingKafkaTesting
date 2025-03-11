8541fa3adfe30e91b4885b16f39028d3fdd7222d
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-03-11 19:39:49 +0530

    Create KafkaConsumer.java

---
+++ kafka-integration-test/src/main/java/com/example/kafka/KafkaConsumer.java
@@ -0,0 +1,13 @@
+package com.example.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "test-topic", groupId = "test-group")
+    public void listen(String message) {
+        System.out.println("Consumed message: " + message);
+    }
+}
