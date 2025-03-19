daa7d3b27a4a7a1559b3f1b3c867e94d66b3a96a
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-03-19 23:20:12 +0530

    Update 3.startKafka.sh

--- 3.startKafka.sh
+++ 3.startKafka.sh
@@ -1,4 +1,4 @@
-cd CNATSTrainingKafkaTesting/
+
 cd kafka
 bin/kafka-topics.sh --create --topic test-topic --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
 bin/kafka-server-start.sh config/server.properties
