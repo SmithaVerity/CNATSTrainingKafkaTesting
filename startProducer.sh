a9d10d6823df1acf43c0333cbb05429c839e0cc9
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-03-19 10:14:19 +0530

    Create startProducer.sh

---
+++ startProducer.sh
@@ -0,0 +1,7 @@
+bin/kafka-topics.sh --list --bootstrap-server localhost:9092
+mkdir spring-kafka-app && cd spring-kafka-app
+mvn archetype:generate -DgroupId=com.example -DartifactId=kafka-app -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+cd kafka-app
+
+
+bin/kafka-console-producer.sh --topic test-topic --bootstrap-server localhost:9092
