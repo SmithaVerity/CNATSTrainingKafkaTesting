8764005279f5aa28b819412c21bf7ede39a09ec4
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-03-19 23:58:01 +0530

    Update 4.startProducer.sh

--- 4.startProducer.sh
+++ 4.startProducer.sh
@@ -2,7 +2,7 @@ cd kafka
 bin/kafka-topics.sh --list --bootstrap-server localhost:9092
 mkdir spring-kafka-app && cd spring-kafka-app
 mvn archetype:generate -DgroupId=com.example -DartifactId=kafka-app -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
-cd kafka-app
+cd kafka
 
 
 bin/kafka-console-producer.sh --topic test-topic --bootstrap-server localhost:9092
