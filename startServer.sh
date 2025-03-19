59dc64a2999889c2ac2900c07e28dcc4d1a7f510
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-03-19 10:23:46 +0530

    Update and rename startServer.sh to startZooServer.sh

--- startServer.sh
+++
@@ -1,4 +0,0 @@
-bin/zookeeper-server-start.sh config/zookeeper.properties
-bin/kafka-server-start.sh config/server.properties
-bin/kafka-topics.sh --create --topic test-topic --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
-bin/kafka-console-consumer.sh --topic test-topic --from-beginning --bootstrap-server localhost:9092
